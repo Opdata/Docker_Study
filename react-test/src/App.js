8edaccbafbcd604e8bb338bb414b3e994d0baf45
Opdata <pb03641@gmail.com>
2020-10-29 18:06:23 +0900

    Compose Test

--- react-test/src/App.js
+++ react-test/src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return <div className="App">React Test</div>;
+  return <div className="App">Compose Hotload Test</div>;
 }
 
 export default App;
